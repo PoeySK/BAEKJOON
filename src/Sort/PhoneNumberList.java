0938d74c41e1e6824f791442671c181e8ae00d34
YeopMan <201802044@o.cnu.ac.kr>
2021-09-11 23:27:28 +0900

    PhoneNumberList solve

--- src/Sort/PhoneNumberList.java
+++ src/Sort/PhoneNumberList.java
@@ -3,24 +3,32 @@ package Sort;
 import java.io.*;
 import java.util.*;
 
+// startsWith() 함수 사용 버전
 public class PhoneNumberList {
+    static String[] numList; // 맨 앞에 0이 올 수도 있기 때문에 String으로 배열을 선언
+    static int n;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int t = Integer.parseInt(br.readLine());
+        int t = Integer.parseInt(br.readLine()); // 테스트 케이스
+        while (t-- > 0) {
+            n = Integer.parseInt(br.readLine()); // 번호 개수
+            numList = new String[n];
 
-        for (int i = 0; i < t; i++) {
-            int n = Integer.parseInt(br.readLine());
-            int[] list = new int[n];
-            for (int j = 0; j < n; j++) {
-                int number = Integer.parseInt(br.readLine());
-                list[j] = number;
+            for (int i = 0; i < n; i++) {
+                String s = br.readLine();
+
+                numList[i] = s;
             }
-            if (isCheck(list)) {
-                bw.write("YES");
-            } else {
-                bw.write("NO");
+
+            Arrays.sort(numList); // 오름차순 정렬
+
+            if (isCheck()) { // true가 반환되면 중복된 것이 없음.
+                bw.write("YES\n");
+            } else { // false가 반환되면 중복된 것이 있음.
+                bw.write("NO\n");
             }
 
         }
@@ -30,10 +38,38 @@ public class PhoneNumberList {
         bw.close();
     }
 
-    public static boolean isCheck(int[] list) {
-        Arrays.sort(list);
-        for (int i = 0; i < list.length; i++) {
+    public static boolean isCheck() {
+        for (int i = 0; i < n - 1; i++) {
+            /*
+            * a.startWith(b): b에 있는 문자열이 a에 포함되어 있으면 true를 반환
+            * 오름차순 정렬이기 때문에 옆만 검사를 해주면 됨.
+            * (ex1. [12 123 54 127] | 이렇게 되어 있으면 정렬은 [12 123 127 54] 로 된다.
+            * (ex2. [12 564 123 12 127] | 이렇게 되어 있으면 정렬은 [12 12 123 127 564]로 되어 바로 왼쪽만 비교하면 된다.
+             */
+            if (numList[i + 1].startsWith(numList[i])) {
+                return false;
+            }
         }
+
         return true;
     }
+
 }
+/*
+
+1
+3
+911
+97654599
+91156877
+
+
+1
+5
+113
+12340
+123440
+12345
+98346
+
+ */
