e3e89ce4ac106a05f82e92d6cb0e72df82989833
PoeySK <piz115@naver.com>
2024-10-21 21:47:56 +0900

    No.1300 solved with "Java" _241021_

--- src/BSearch/KthNumber.java
+++ src/BSearch/KthNumber.java
@@ -4,29 +4,43 @@ import java.io.*;
 import java.util.*;
 
 public class KthNumber {
+    static int N, k;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int N = Integer.parseInt(br.readLine());
-        Set<Long> b = new TreeSet<>();
+        /* 입력 */
+        N = Integer.parseInt(br.readLine());
+        k = Integer.parseInt(br.readLine());
 
-        for (int i = 1; i <= N; i++) {
-            for (int j = 1; j <= N; j++) {
-                b.add((long)i*j);
+        /* 작동 */
+        int result = run();
+
+        /* 출력 */
+        bw.write(result + "\n");
+        bw.close();
+    }
+
+    private static int run() {
+        int low = 1;
+        int high = k;
+        int mid;
+        while (low <= high) {
+            mid = (low + high) / 2;
+
+            int count = 0;
+            for (int i = 1; i <= N; i++) { // mid 보다 작은 값 찾기
+                count += Math.min(mid / i, N); // 최대 행의 개수까지
+            }
+
+            if (count < k) {
+                low = mid + 1;
+            } else { // count >= k
+                high = mid - 1;
             }
         }
 
-        int k = Integer.parseInt(br.readLine());
-        int index = 0;
-        Iterator<Long> iter = b.iterator();
-        while(iter.hasNext()){
-            index++;
-            if(index == k) {
-                System.out.println(iter.next());
-                break;
-            }
-            iter.next();
-        }
-
+        return low;
     }
 }
