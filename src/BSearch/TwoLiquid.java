989ff57b95f9776a54255a658a119f65d2a10d01
PoeySK <piz115@naver.com>
2024-10-24 21:32:35 +0900

    No.2470 solved with "Java" _241024_

--- src/BSearch/TwoLiquid.java
+++ src/BSearch/TwoLiquid.java
@@ -4,33 +4,76 @@ import java.io.*;
 import java.util.*;
 
 public class TwoLiquid {
-    static int N;
-    static long[] Acid, Alkaline;
-    static int zero;
+    static int K, left, right;
+    static int[] liquids;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        N = Integer.parseInt(br.readLine());
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        String[] liquid = br.readLine().split(" ");
+        /* 입력 */
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        K = Integer.parseInt(st.nextToken());
+        liquids = new int[K];
+        st = new StringTokenizer(br.readLine());
+        int alk = 0, aci = 0;
+        for (int i = 0; i < K; i++) {
+            liquids[i] = Integer.parseInt(st.nextToken());
+            if (liquids[i] < 0) alk++;
+            else aci++;
+        }
+        br.close();
 
-        for (int i = 0; i < N; i++) {
-            if (Long.parseLong(liquid[i]) > 0) {
-                Acid[i] = Long.parseLong(liquid[i]);
-            }
-            else if (Long.parseLong(liquid[i]) < 0){
-                Alkaline[i] = Long.parseLong(liquid[i]);
-            } else {
-                zero = Integer.parseInt(liquid[i]);
+        /* 작동 */
+        Arrays.sort(liquids);
+
+        if (alk == 0) { // only 산성
+            bw.write(liquids[0] + " " + liquids[1]);
+            bw.close();
+            return;
+        }
+        if (aci == 0) { // only 알칼리
+            left = liquids[K - 2];
+            right = liquids[K - 1];
+            bw.write(liquids[K - 2] + " " + liquids[K - 1]);
+            bw.close();
+            return;
+        }
+        if (alk > 0 && aci > 0) run();
+
+        /* 출력 */
+        bw.write(liquids[left] + " " + liquids[right] + "\n");
+        bw.close();
+    }
+
+    private static void run() {
+        int low = 0;
+        int high = K - 1;
+        int min = Integer.MAX_VALUE;
+        left = low;
+        right = high;
+        while (low < high) {
+            int sum = liquids[low] + liquids[high];
+            int abs = Math.abs(sum);
+            if (sum > 0) {
+                if(abs < min) {
+                    min = abs;
+                    left = low;
+                    right = high;
+                }
+                high--;
+            } else if (sum < 0) {
+                if(abs < min) {
+                    min = abs;
+                    left = low;
+                    right = high;
+                }
+                low++;
+            } else { // sum == 0
+                left = low;
+                right = high;
+                return;
             }
         }
-        Arrays.sort(Acid);
-        Arrays.sort(Alkaline);
     }
-
-    static void getSpecial(){
-
-    }
-
-
 }
